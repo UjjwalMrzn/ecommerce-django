3fc3af02a25020081bf0a115cfec3a594aa8d015
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-09-12 16:07:53 +0545

    api,axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,24 @@
-// import React, {useState , useEffect}  from 'react'
-import React from 'react'
+import React, {useState , useEffect}  from 'react'
+// import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-// import axios from 'axios'
+import axios from 'axios'
 
-import products from '../products'
+// import products from '../products'
 
 
 
 
 function HomeScreen() {
-  // const[products, setProducts] = useState([])
+  const[products, setProducts] = useState([])
 
-  // useEffect(() => {
-  //   async function fetchProducts() {
-  //     const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
-  //     setProducts(data)
-  //   }
-  //   fetchProducts()
-  // }, []) 
+  useEffect(() => {
+    async function fetchProducts() {
+      const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, []) 
 
   return (
     <div>
